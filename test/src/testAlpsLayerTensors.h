834924d67cd828163baed305427f2ad70966ab8c
YannCobigo <yann.cobigo@yahoo.com>
2021-01-09 18:40:36 -0800

    Add AlpsLayerTensor unit test.

---
+++ test/src/testAlpsLayerTensors.h
@@ -0,0 +1,27 @@
+#include "gtest/gtest.h"
+
+// The fixture for testing class Subjects.
+class LayerTensorsTest : public testing::Test {
+
+protected:
+
+    // You can do set-up work for each test here.
+    LayerTensorsTest();
+
+    // You can do clean-up work that doesn't throw exceptions here.
+    virtual ~LayerTensorsTest();
+
+    // If the constructor and destructor are not enough for setting up
+    // and cleaning up each test, you can define the following methods:
+
+    // Code here will be called immediately after the constructor (right
+    // before each test).
+    virtual void SetUp();
+
+    // Code here will be called immediately after each test (right
+    // before the destructor).
+    virtual void TearDown();
+
+    // The mock bar library shaed by all tests
+    //MockBar m_bar;
+};
