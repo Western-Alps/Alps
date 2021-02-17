45dadc02ebda245314e892a1c700ebc263a01e6a
YannCobigo <yann.cobigo@yahoo.com>
2021-02-16 23:22:04 -0800

    Add the test structure for the stochastic gradient descent (SGD).

---
+++ test/src/testAlpsSGD.cpp
@@ -0,0 +1,82 @@
+#include "testAlpsSGD.h"
+#include <iostream>
+#include <memory>
+// Eigen
+#include <Eigen/Core>
+#include <Eigen/Eigen>
+// ITK
+#include "ITKHeaders.h"
+#include "AlpsCostFunction.h"
+#include "AlpsActivations.h"
+
+//using ::testing::Return;
+
+SGCTest::SGCTest() {
+    // Have qux return true by default
+    //ON_CALL(m_bar,qux()).WillByDefault(Return(true));
+    // Have norf return false by default
+    //ON_CALL(m_bar,norf()).WillByDefault(Return(false));
+}
+
+SGCTest::~SGCTest() {};
+
+void SGCTest::SetUp() {};
+
+void SGCTest::TearDown() {};
+// CostFunction function
+TEST_F(SGCTest, ByDefaultCostFunctionLSE_L) {
+  // Loss
+  Alps::LeastSquarreEstimate< double > LSE;
+  //
+  //
+  int N = 3;
+  std::shared_ptr< double > shv1( new double[N], std::default_delete< double [] >() );
+  std::shared_ptr< double > shv2( new double[N], std::default_delete< double [] >() );
+  //  
+  shv1.get()[0] = 1.; shv1.get()[1] = 2.; shv1.get()[2] = 3.; 
+  shv2.get()[0] = 4.; shv2.get()[1] = 5.; shv2.get()[2] = 6.; 
+  //
+  //
+  EXPECT_EQ( LSE.L(shv1.get(), shv2.get(), N), N * 9. );
+}
+// Derivative of the activation function
+TEST_F(SGCTest, ByDefaultCostFunctionTanh_df) {
+  // Loss
+  Alps::LeastSquarreEstimate< double > LSE;
+  //
+  //
+  int N = 3;
+  std::shared_ptr< double > shv1( new double[N], std::default_delete< double [] >() );
+  std::shared_ptr< double > shv2( new double[N], std::default_delete< double [] >() );
+  std::shared_ptr< double > shv3( new double[N], std::default_delete< double [] >() );
+  //  
+  shv1.get()[0] = 1.; shv1.get()[1] = 2.; shv1.get()[2] = 3.; 
+  shv2.get()[0] = 4.; shv2.get()[1] = 5.; shv2.get()[2] = 6.; 
+  shv3.get()[0] = 1.; shv3.get()[1] = 2.; shv3.get()[2] = 3.;
+  //
+  double val =0.;
+  for ( int i = 0 ; i < N ; i++ )
+    {
+//      std::cout << "dL[" << i << "] = "
+//		<< LSE.dL(shv1.get(), shv2.get(), shv3.get(), N).get()[i]
+//		<< std::endl;
+      val += LSE.dL(shv1.get(), shv2.get(), shv3.get(), N).get()[i];
+    }
+  
+  //
+  //
+  EXPECT_EQ( val, - static_cast< double >(N * (1+2+3)) );
+}
+
+//TEST_F(ImageTest, ByDefaultBazFalseIsFalse) {
+//    Image foo(m_bar);
+//    EXPECT_EQ(foo.baz(false), false);
+//}
+//
+//TEST_F(ImageTest, SometimesBazFalseIsTrue) {
+//    Image foo(m_bar);
+//    // Have norf return true for once
+//    EXPECT_CALL(m_bar,norf()).WillOnce(Return(true));
+//    EXPECT_EQ(foo.baz(false), true);
+//}
+
